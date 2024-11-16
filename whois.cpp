cfaa9c3a8fb9be9f7604be4b88bd68da8236759b
Masaya Takahashi <takamwsa@gmail.com>
2024-11-16 20:00:30 +0900

    upd: shorten code

--- whois.cpp
+++ whois.cpp
@@ -1,18 +1,15 @@
-#include <iostream>
-#include <cstdlib>
 #include <string>
-using namespace std;
 
 int main(int argc, char *argv[]) {
-    string options;
+    std::string options;
 
     for (size_t i = 1; i < argc; i++)
     {
-        options += argv[i];
         options += " ";
+        options += argv[i];
     }
 
-    string exec = "wsl whois " + options;
+    std::string exec = "wsl whois" + options;
     system(exec.c_str());
 
     return 0;
